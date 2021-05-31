a0424d13153e3dcfc7c6562bc637597647e1fb7f
Antonio Papa <antoniopapa1991@gmail.com>
2021-05-31 15:51:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const Pusher = require("pusher");
+
+const pusher = new Pusher({
+    appId: "",
+    key: "",
+    secret: "",
+    cluster: "",
+    useTLS: true
+});
+
+const app = express();
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}))
+
+app.use(express.json())
+
+app.post('/api/messages', async (req, res) => {
+    await pusher.trigger("chat", "message", {
+        username: req.body.username,
+        message: req.body.message
+    });
+
+    res.json([]);
+})
+
+console.log('listening to port 8000');
+app.listen(8000)
